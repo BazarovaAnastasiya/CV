b971b219d7ffa5453413165a9d972278f6a15d8f
BazarovaAnastasiya <anastasiya.bazarova.85@gmail.com>
2022-09-06 21:54:52 +0300

    fix: html

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,61 @@ function onTabClick(item) {
     }
   });
 }
+const form = document.getElementById("form");
+form.addEventListener("submit", formSend);
+
+async function formSend(e) {
+  e.preventDefault();
+  let error = formValidate(form);
+  if (error === 0) {
+    console.log()
+    if (response.ok) {
+      console.log(result.message);
+      form.reset();
+    } else {
+      alert("");
+    }
+  } else {
+    console.log("Form is NOT sent.");
+  }
+}
+
+function formValidate(form) {
+  let error = 0;
+  let formReq = document.querySelectorAll("._req");
+
+  for (let index = 0; index < formReq.length; index++) {
+    const input = formReq[index];
+    RemoveError(input);
+
+    if (input.classList.contains("email")) {
+      if (emailTest(input)) {
+        AddError(input);
+        error++;
+      }
+    } else {
+      if (input.value === "") {
+        AddError(input);
+        error++;
+        console.log("No");
+      }
+    }
+  }
+  return error;
+}
+
+function AddError(input) {
+  input.parentElement.classList.add("_error");
+  input.classList.add("_error");
+}
+function RemoveError(input) {
+  input.parentElement.classList.remove("_error");
+  input.classList.remove("_error");
+}
+
+function emailTest(input) {
+  return !/^\w+([\.-]?\w+)*@(\w+[\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+}
+
+
+
